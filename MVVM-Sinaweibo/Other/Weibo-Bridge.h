e3f026bed2fc9887306b3299c813163ab88e8871
liuyanchi <liuyanchi@yongche.com>
2018-05-20 17:29:28 +0800

    Swift-Create
    Swift-Create

---
+++ MVVM-Sinaweibo/Other/Weibo-Bridge.h
@@ -0,0 +1,13 @@
+//
+//  Weibo-Bridge.h
+//  MVVM-Sinaweibo
+//
+//  Created by liuyanchi on 2018/5/20.
+//  Copyright © 2018年 yidaoyongche. All rights reserved.
+//
+
+#ifndef Weibo_Bridge_h
+#define Weibo_Bridge_h
+
+
+#endif /* Weibo_Bridge_h */
---
+++ Podfile
@@ -0,0 +1,9 @@
+ platform :ios, '9.0'
+
+target 'MVVM-Sinaweibo' do
+
+        pod 'AFNetworking', '~> 3.0'
+        pod 'FMDB'
+        pod 'ReactiveCocoa','2.5.0'
+
+end
